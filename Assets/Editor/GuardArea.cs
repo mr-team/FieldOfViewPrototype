93498a6f9bf916830a30c44d6bc90317b7fc255e
Jonas Refseth <jonas@oisann.net>
2016-01-27 17:02:15 +0100

    Guard Area and Field Of View

---
+++ Assets/Editor/GuardArea.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+[CustomEditor(typeof(GuardPatrol))]
+public class GuardArea : Editor {
+
+    void OnSceneGUI() {
+        GuardPatrol guardArea = (GuardPatrol) target;
+        Handles.color = Color.cyan;
+
+        Handles.DrawLine(guardArea.cornerA, new Vector3(guardArea.cornerA.x, (guardArea.cornerA.y + guardArea.cornerB.y) / 2, guardArea.cornerB.z));
+        Handles.DrawLine(guardArea.cornerA, new Vector3(guardArea.cornerB.x, (guardArea.cornerB.y + guardArea.cornerA.y) / 2, guardArea.cornerA.z));
+
+        Handles.DrawLine(guardArea.cornerB, new Vector3(guardArea.cornerA.x, (guardArea.cornerA.y + guardArea.cornerB.y) / 2, guardArea.cornerB.z));
+        Handles.DrawLine(guardArea.cornerB, new Vector3(guardArea.cornerB.x, (guardArea.cornerB.y + guardArea.cornerA.y) / 2, guardArea.cornerA.z));
+    }
+
+}
---
+++ Assets/Scripts/GuardPatrol.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GuardPatrol : MonoBehaviour {
+
+    public float moveSpeed = 6;
+
+    public Vector3 cornerA = new Vector3(0, 0, 0);
+    public Vector3 cornerB = new Vector3(0, 0, 0);
+
+    Rigidbody rigidbody;
+    Camera viewCamera;
+    Vector3 velocity;
+    Vector3 target;
+
+    void Start() {
+        rigidbody = GetComponent<Rigidbody>();
+        viewCamera = Camera.main;
+        target = RandomPositionInRectangle();
+    }
+
+    void FixedUpdate() {
+        float distanceToTarget = Vector3.Distance(transform.position, target);
+        if(distanceToTarget > 1f) {
+            float step = moveSpeed * Time.fixedDeltaTime;
+            transform.position = Vector3.MoveTowards(transform.position, target, step);
+            transform.LookAt(target + Vector3.up * transform.position.y);
+        } else {
+            target = RandomPositionInRectangle();
+        }
+    }
+
+    Vector3 RandomPositionInRectangle() {
+        Vector3 result = new Vector3(Random.Range(cornerA.x, cornerB.x), Random.Range(cornerA.y, cornerB.y), Random.Range(cornerA.z, cornerB.z));
+        Debug.Log(result);
+        return result;
+    }
+}
